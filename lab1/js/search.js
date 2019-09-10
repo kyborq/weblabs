b9b1978e126cdebfd4f9e1f8b7bf6cb5ab0ec6a8
Konstantin Podyganov <podyganov01@gmail.com>
2019-09-10 16:03:13 +0300

    Fixed issue #1

--- lab1/js/search.js
+++ lab1/js/search.js
@@ -1,33 +1,33 @@
 // Start search function
 function startSearch() {
   // Get search object
-  searchField = document.getElementById("search");
+  searchField = document.getElementById('search');
 
   // Get card object
-  card = document.getElementsByClassName("column")
+  var card = document.getElementsByClassName('column')
   // Get card content array
-  cardContents = document.getElementsByClassName("content")
+  var cardContents = document.getElementsByClassName('content')
   // Get card count
-  cardsCount = cardContents.length;
+  var cardsCount = cardContents.length;
 
   // If search field is not empty
-  if (searchField.value != "") {
+  if (searchField.value !== '') {
     // Run through the array of cards
     for (var i = 0; i < cardsCount; i++) {
       // And compare search field text with card content text
-      if (cardContents[i].innerText == searchField.value) {
-        // If success -> show the desired card
-        card[i].style.display = "block";
+      if (cardContents[i].innerText.indexOf(searchField.value) >= 0) {
+        // If result more or equals 0 -> show the desired card
+        card[i].style.display = 'block';
       } else {
-        // If fail -> hide unuseful card
-        card[i].style.display = "none";
+        // Or else -> hide unuseful card
+        card[i].style.display = 'none';
       }
     }
   } else {
     // If search button clicked and search field is empty
     // Run through the array of cards and show all card
     for (var i = 0; i < cardsCount; i++) {
-      card[i].style.display = "block";
+      card[i].style.display = 'block';
     }
   }
 }
