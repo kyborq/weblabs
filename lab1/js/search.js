4500b92c9e1156e6b81eacbeda3f058109d62154
Konstantin Podyganov <podyganov01@gmail.com>
2019-09-10 12:01:32 +0300

    Added comments to get life easier

--- lab1/js/search.js
+++ lab1/js/search.js
@@ -1,21 +1,31 @@
+// Start search function
 function startSearch() {
+  // Get search object
   searchField = document.getElementById("search");
 
+  // Get card object
   card = document.getElementsByClassName("column")
+  // Get card content array
   cardContents = document.getElementsByClassName("content")
+  // Get card count
   cardsCount = cardContents.length;
 
-  sampleText = "Lorem";
-
+  // If search field is not empty
   if (searchField.value != "") {
+    // Run through the array of cards
     for (var i = 0; i < cardsCount; i++) {
+      // And compare search field text with card content text
       if (cardContents[i].innerText == searchField.value) {
+        // If success -> show the desired card
         card[i].style.display = "block";
       } else {
+        // If fail -> hide unuseful card
         card[i].style.display = "none";
       }
     }
   } else {
+    // If search button clicked and search field is empty
+    // Run through the array of cards and show all card
     for (var i = 0; i < cardsCount; i++) {
       card[i].style.display = "block";
     }
