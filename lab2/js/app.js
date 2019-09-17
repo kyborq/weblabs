ecd1ad1a62b86b0a563fda2e60d11eb1888d3f97
Konstantin Podyganov <podyganov01@gmail.com>
2019-09-17 15:48:36 +0300

    Added files via upload

--- lab2/js/app.js
+++ lab2/js/app.js
@@ -1,56 +1,65 @@
-var ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
+window.onload = function () {
+  var btn = document.getElementById('button');
+  var txtArea = document.getElementById('textarea');
+
+  // add task handler
+  btn.onclick = function () {
+    // add element to UI
+    var taskContent = txtArea.value;
+    var containerToDo = document.getElementById('container-to-do');
+    var newTask = document.createElement('div');
+    newTask.classList.add('new', 'checkbox');
+    containerToDo.append(newTask);
+    var label = document.createElement('label');
+    var check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    label.append(check);
+    newTask.append(label);
+    var text = document.createTextNode(taskContent);
+    label.append(text);
+    var icon = document.createElement('i');
+    icon.classList.add('glyphicon', 'glyphicon-trash', 'icon', 'remove-card');
+    newTask.append(icon);
+    txtArea.value = "";
+
+    // register events here:
+
+    // complete task
+    // TODO: labeled - not valid name for CSS and context
+    check.onclick = function () {
+      label.classList.toggle('labeled');
+    };
+
+    // TODO: delete task event
+    icon.onclick = function () {
+      console.log('delete');
+    };
+  };
+
+  // TODO: complete all handler here
+  /*
+    var completeAll = document.getElementById('complete-all');
+    completeAll.onclick = function () {
+      console.log('complete all');
+    };
+  */
+
+
+  // TODO: filter tasks
+  /*
+    var filterShowAll = document.getElementById('filter-show-all');
+    filterShowAll.onclick = function () {
+      console.log('show all');
+    };
+
+    var filterShowCompleted = document.getElementById('filter-show-completed');
+    filterShowCompleted.onclick = function () {
+      console.log('show completed');
+    };
+
+    var filterShowCompleted = document.getElementById('filter-show-removed');
+    filterShowRemoved.onclick = function () {
+      console.log('show removed');
+    };
+  */
 }
-
-ready(() => {
-  var btn = document.querySelector('#btn');
-  var textarea = document.querySelector('#textarea');
-
-  btn.addEventListener('click', (e) => {
-    addTask();
-    textarea.value = '';
-  });
-
-  function addTask() {
-    if (textarea.value !== '') {
-      var taskContent = textarea.value;
-      var containerToDo = document.querySelector('#container-to-do');
-      var newTask = document.createElement('div');
-      newTask.classList.add('new');
-      newTask.classList.add('checkbox');
-      containerToDo.appendChild(newTask);
-      var label = document.createElement('label');
-      var check = document.createElement('input');
-      check.setAttribute('type', 'checkbox');
-      label.appendChild(check);
-      newTask.append(label);
-      var text = document.createTextNode(taskContent);
-      label.appendChild(text);
-      var icon = document.createElement('i');
-      icon.classList.add('glyphicon');
-      icon.classList.add('glyphicon-trash');
-      icon.classList.add('icon');
-      newTask.appendChild(icon);
-
-      textarea.value = '';
-
-      // ???
-      check.click(function() {
-        labeledTask();
-      });
-
-      function labeledTask() {
-        this.classList.toggle('labeled');
-      }
-    }
-  }
-
-  function deleteTask() {
-
-  }
-
-  function comlpeteAllTasks() {
-
-  }
-});
