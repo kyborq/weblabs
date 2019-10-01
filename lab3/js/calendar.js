aa4a020a3b3581ef36cba36ec40e22077166686f
Konstantin Podyganov <podyganov01@gmail.com>
2019-10-01 16:07:58 +0300

    Added calendar logic

--- lab3/js/calendar.js
+++ lab3/js/calendar.js
@@ -1,4 +1,15 @@
 window.onload = function() {
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  var dateCells = document.querySelectorAll('.day');
+  for (let cell of dateCells) {
+    if (cell.innerText == currentDay) {
+      if (!cell.parentElement.classList.contains('other-month')) {
+        cell.classList.toggle('today');
+      }
+    }
+  }
+
   var toggleNight = document.getElementById('night-mode');
   toggleNight.onclick = function() {
     var calendarWindow = document.getElementById('calendar-app');
