d0a059e9a6c395895c1b8b95fc379cb40bc2480f
Konstantin Podyganov <podyganov01@gmail.com>
2019-10-15 15:39:20 +0300

    Navigation for elements aded

--- lab4/js/total.js
+++ lab4/js/total.js
@@ -3,13 +3,8 @@ window.onload = function() {
   let currentRow = 2;
 
   for (var i = 0; i < rows.length; i++) {
-    rows[i].onclick = function(event) {
-      currentRow = event.target.parentElement.children[0].innerText;
-      console.log(currentRow, event.target.parentElement);
-
-      if (i == currentRow) {
-        rows[i].classList.toggle('selected');
-      }
+    if (i == currentRow) {
+      rows[i].classList.toggle('selected');
     }
   }
 
