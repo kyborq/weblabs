c814310c7dd9f9d0ebd9caeaa331f97a7b333061
Konstantin Podyganov <podyganov01@gmail.com>
2019-10-22 17:16:54 +0300

    Created object for Total Commander

--- lab4/TotalCommander/js/total.js
+++ lab4/TotalCommander/js/total.js
@@ -1,65 +1,72 @@
-window.onload = function() {
-  let sideDir = document.querySelectorAll('.left-side')[0].children[0].children;
+var totalCommander = {
+  currentIndex : 0,
+  sideDir : document.querySelectorAll('.left-side')[0].children[0].children,
+  count : 0,
 
-  let currentIndex = 0;
-  let count = sideDir.length;
+  init() {
+    this.count = this.sideDir.length;
+    this.cursorMove();
 
-  cursorMove();
-
-  // arrow button events
-  document.addEventListener('keydown', function(event) {
-    // left button pressed
-    if (event.keyCode === 37) {
-      clearSelection();
-      currentIndex = 0;
-      sideDir = document.querySelectorAll('.left-side')[0].children[0].children;
-      count = sideDir.length;
-      cursorMove();
-      // console.log("Left tab active");
-    }
-
-    // right button pressed
-    if (event.keyCode === 39) {
-      clearSelection();
-      currentIndex = 0;
-      sideDir = document.querySelectorAll('.right-side')[0].children[0].children;
-      count = sideDir.length;
-      cursorMove();
-      // console.log("Right tab active");
-    }
-
-    // up button pressed
-    if (event.keyCode === 38) {
-      if (currentIndex > 0) {
-        currentIndex -= 1;
-        cursorMove();
-        // console.log("(" + currentIndex + "/" + count + ")");
+    console.log('Initialized');
+    // arrow button events
+    document.addEventListener('keydown', function(event) {
+      // left button pressed
+      if (event.keyCode === 37) {
+        clearSelection();
+        this.currentIndex = 0;
+        this.sideDir = document.querySelectorAll('.left-side')[0].children[0].children;
+        this.count = this.sideDir.length;
+        this.cursorMove();
+        console.log("Left tab active");
       }
-    }
 
-    // down button pressed
-    if (event.keyCode === 40) {
-      if (currentIndex < count - 1) {
-        currentIndex += 1;
-        cursorMove();
-        // console.log("(" + currentIndex + "/" + count + ")");
+      // right button pressed
+      if (event.keyCode === 39) {
+        clearSelection();
+        this.currentIndex = 0;
+        this.sideDir = document.querySelectorAll('.right-side')[0].children[0].children;
+        this.count = this.sideDir.length;
+        this.cursorMove();
+        console.log("Right tab active");
       }
-    }
-  });
 
-  function cursorMove() {
-    for (var i = 0; i < sideDir.length; i++) {
-      if (i == currentIndex) {
-        sideDir[i].classList.add('selected');
+      // up button pressed
+      if (event.keyCode === 38) {
+        if (this.currentIndex > 0) {
+          this.currentIndex -= 1;
+          this.cursorMove();
+          console.log("(" + currentIndex + "/" + count + ")");
+        }
+      }
+
+      // down button pressed
+      if (event.keyCode === 40) {
+        if (this.currentIndex < this.count - 1) {
+          this.currentIndex += 1;
+          this.cursorMove();
+          console.log("(" + currentIndex + "/" + count + ")");
+        }
+      }
+    });
+  },
+
+  cursorMove() {
+    for (var i = 0; i < this.sideDir.length; i++) {
+      if (i == this.currentIndex) {
+        this.sideDir[i].classList.add('selected');
       } else {
-        sideDir[i].classList.remove('selected');
+        this.sideDir[i].classList.remove('selected');
       }
     }
-  }
+  },
 
-  function clearSelection() {
-    for (var i = 0; i < sideDir.length; i++) {
-      sideDir[i].classList.remove('selected');
+  clearSelection() {
+    for (var i = 0; i < this.sideDir.length; i++) {
+      this.sideDir[i].classList.remove('selected');
     }
   }
+};
+
+window.onload = function() {
+  totalCommander.init();
 }
