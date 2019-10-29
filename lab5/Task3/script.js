f082955a86595818233aadd5d54ce280f1f4e038
Konstantin <k.podyganov@mail.ru>
2019-10-29 13:47:48 +0300

    Fixed errors

--- lab5/Task3/script.js
+++ lab5/Task3/script.js
@@ -1,24 +1,26 @@
 function pulloutArray(arr) {
-  var resultArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    // if (isNaN(arr[i]) === false && arr[i] !== null) {
-    if (typeof arr[i] === 'object' && !isNaN(arr[i]) && arr[i] !== null) {
-      for (var j = 0; j < arr[i].length; j++) {
-        resultArr.push(arr[i][j]);
+  if (Array.isArray(arr)) {
+    var resultArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      // if (isNaN(arr[i]) === false && arr[i] !== null) {
+      if (Array.isArray(arr[i]) && !isNaN(arr[i]) && arr[i] !== null) {
+        for (var j = 0; j < arr[i].length; j++) {
+          resultArr.push(arr[i][j]);
+        }
+      } else if (!isNaN(arr[i]) && arr[i] !== null) {
+        resultArr.push(arr[i]);
       }
-    } else if (!isNaN(arr[i]) && arr[i] !== null) {
-      resultArr.push(arr[i]);
+      // }
     }
-    // }
+  } else {
+    return null;
   }
-  arr = resultArr;
 
-  console.log(arr);
-  // return arr;
+  return resultArr;
 }
 
-pulloutArray([1, 2, 3]);
-pulloutArray([]);
-pulloutArray([1, [2, 3, 4], 5]);
-pulloutArray([1, [2, 3, 4], 5, [1]]);
-pulloutArray([1, [1], null, NaN, ['test']]);
+console.log(pulloutArray([1, 2, 3]));
+console.log(pulloutArray([]));
+console.log(pulloutArray([1, [2, 3, 4], 5]));
+console.log(pulloutArray([1, [2, 3, 4], 5, [1]]));
+console.log(pulloutArray([1, [1], null, NaN, ['test']]));
--- lab5/Task4/script.js
+++ lab5/Task4/script.js
@@ -1,17 +1,12 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  // First interval
-  var start1 = new Date('Mon Oct 10 2019 ' + timeRange1[0] + ':00');
-  var end1 = new Date('Mon Oct 10 2019 ' + timeRange1[1] + ':00');
-  // Second interval
-  var start2 = new Date('Mon Oct 10 2019 ' + timeRange2[0] + ':00');
-  var end2 = new Date('Mon Oct 10 2019 ' + timeRange2[1] + ':00');
-
-  if ((start1 > start2 && start1 < end2) || (start2 > start1 && start2 < end1)) {
-    console.log(true);
-  } else {
-    console.log(false);
+  if (Array.isArray(timeRange1) &&  Array.isArray(timeRange2)) {
+    if ((timeRange1[0] > timeRange2[0] && timeRange1[0] < timeRange2[1]) || (timeRange2[0] > timeRange1[0] && timeRange2[0] < timeRange1[1])) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
-isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00']); // return false
-isTimeRangesIntersect(['18:30', '19:30'], ['19:00', '21:00']); // return true
+console.log(isTimeRangesIntersect(['18:30', '19:30'], ['19:00', '21:00'])); // return true
+console.log(isTimeRangesIntersect(['08:30', '09:30'], ['10:30', '12:00'])); // return false
