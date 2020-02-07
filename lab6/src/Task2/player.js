083db2f2edc817f95df1810b4ba2fc166f2a346d
Konstantin <k.podyganov@mail.ru>
2020-02-07 16:42:03 +0300

    Added Music Player class

--- lab6/src/Task2/player.js
+++ lab6/src/Task2/player.js
@@ -1,13 +1,15 @@
-function Song() {
-  // название, длительность
+function Song(songName, songLength) {
+  this.songName = songName; // Название
+  this.songLength = songLength; // Длительность
 }
 
-function PlayerList() {
-  // хранит список песен
+function PlayerList(song) {
+  this.songs = [song]; // Хранит список песен
 }
 
 function Player(playerList) {
-  this.currentTrack = "song.mp3";
+  this.currentTrackId = 0;
+  this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
   this.status = "stop";
   this.playerList = playerList;
 
@@ -20,7 +22,7 @@ function Player(playerList) {
   };
 
   this.stop = function() {
-    this.status = "play";
+    this.status = "stop";
   };
 
   this.pause = function() {
@@ -28,10 +30,39 @@ function Player(playerList) {
   };
 
   this.next = function() {
-    // TODO
+    this.currentTrackId += 1;
+
+    if (this.currentTrackId > playerList["songs"][0].length - 1) {
+      this.currentTrackId = 0;
+    }
+
+    // console.log(this.currentTrackId + "/" + (playerList["songs"][0].length - 1));
+    this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
   };
 
   this.prev = function() {
-    // TODO
+    this.currentTrackId -= 1;
+
+    if (this.currentTrackId < 0) {
+      this.currentTrackId = playerList["songs"][0].length - 1;
+    }
+
+    // console.log(this.currentTrackId + "/" + (playerList["songs"][0].length - 1));
+    this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
   };
 }
+
+var playerList = new PlayerList([new Song("Edvard Grieg - Peer Gynt Suite", 102), new Song("Ludwig van Beethoven - Symphony No. 5", 130), new Song("Samuel Barber - Adagio for Strings", 130)]);
+var player = new Player(playerList);
+
+document.getElementById("nextbtn").onclick = function() {
+  player.next();
+  document.getElementById("playerui").innerText = player.display();
+};
+
+document.getElementById("prevbtn").onclick = function() {
+  player.prev();
+  document.getElementById("playerui").innerText = player.display();
+};
+
+document.getElementById("playerui").innerText = player.display();
