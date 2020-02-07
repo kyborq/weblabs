74c5281d9f2b230c9f1b4d03f1bcc82c828d62f1
Konstantin <k.podyganov@mail.ru>
2020-02-07 17:49:11 +0300

    Fixed Task2

--- lab6/src/Task2/player.js
+++ lab6/src/Task2/player.js
@@ -1,68 +1,75 @@
-function Song(songName, songLength) {
+function Song(songName, songDuration) {
   this.songName = songName; // Название
-  this.songLength = songLength; // Длительность
+  this.songDuration = songDuration; // Длительность songDuration
 }
 
-function PlayerList(song) {
-  this.songs = [song]; // Хранит список песен
-}
-
-function Player(playerList) {
+function PlayerList(songsArray) {
   this.currentTrackId = 0;
-  this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
-  this.status = "stop";
-  this.playerList = playerList;
-
-  this.display = function() {
-    return "Track: " + this.currentTrack + " Status: " + this.status;
-  };
-
-  this.play = function() {
-    this.status = "play";
-  };
-
-  this.stop = function() {
-    this.status = "stop";
-  };
-
-  this.pause = function() {
-    this.status = "pause";
-  };
+  this.songs = songsArray; // Хранит список песен
 
   this.next = function() {
     this.currentTrackId += 1;
-
-    if (this.currentTrackId > playerList["songs"][0].length - 1) {
+    if (this.currentTrackId > this.songs.length - 1) {
       this.currentTrackId = 0;
     }
-
-    // console.log(this.currentTrackId + "/" + (playerList["songs"][0].length - 1));
-    this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
   };
 
   this.prev = function() {
     this.currentTrackId -= 1;
-
     if (this.currentTrackId < 0) {
-      this.currentTrackId = playerList["songs"][0].length - 1;
+      this.currentTrackId = this.songs.length - 1;
     }
+  };
 
-    // console.log(this.currentTrackId + "/" + (playerList["songs"][0].length - 1));
-    this.currentTrack = playerList.songs[0][this.currentTrackId].songName;
+  this.getCurrentTrackName = function() {
+    if (this.songs != null) {
+      return this.songs[this.currentTrackId].songName;
+    } else {
+      return 'Empty List!';
+    }
   };
 }
 
-var playerList = new PlayerList([new Song("Edvard Grieg - Peer Gynt Suite", 102), new Song("Ludwig van Beethoven - Symphony No. 5", 130), new Song("Samuel Barber - Adagio for Strings", 130)]);
-var player = new Player(playerList);
+function Player(playerList) {
+  this.status = 'stop';
+  this.playerList = new PlayerList(playerList);
 
-document.getElementById("nextbtn").onclick = function() {
+  this.display = function() {
+    return 'Track: ' + this.playerList.getCurrentTrackName() + ' Status: ' + this.status;
+  };
+
+  this.play = function() {
+    this.status = 'play';
+  };
+
+  this.stop = function() {
+    this.status = 'stop';
+  };
+
+  this.pause = function() {
+    this.status = 'pause';
+  };
+
+  this.next = function() {
+    this.playerList.next();
+  };
+
+  this.prev = function() {
+    this.playerList.prev();
+  };
+}
+
+// var player = new Player([new Song("Edvard Grieg - Peer Gynt Suite", 102), new Song("Ludwig van Beethoven - Symphony No. 5", 130), new Song("Samuel Barber - Adagio for Strings", 130)]);
+var player = new Player();
+
+document.getElementById('nextbtn').onclick = function() {
   player.next();
-  document.getElementById("playerui").innerText = player.display();
+  document.getElementById('playerui').innerText = player.display();
 };
 
-document.getElementById("prevbtn").onclick = function() {
+document.getElementById('prevbtn').onclick = function() {
   player.prev();
-  document.getElementById("playerui").innerText = player.display();
+  document.getElementById('playerui').innerText = player.display();
 };
 
-document.getElementById("playerui").innerText = player.display();
+document.getElementById('playerui').innerText = player.display();
